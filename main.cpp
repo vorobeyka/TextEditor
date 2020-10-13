6f5302edbf5dbab63b0fb9b481587f68c6f80c06
vorobeyka <vorobeyka3222@gmail.com>
2020-10-13 13:58:05 +0300

    change style.css

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-//    QFile file(":/files/images/style.css");
-//    file.open(QFile::ReadOnly);
-//    a.setStyleSheet(file.readAll());
+    QFile file(":/files/images/style.css");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
 
     MainWindow w;
     w.show();
