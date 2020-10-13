3987dfd62bb517cd3d4f0d4f5962737a7c6e7d2a
vorobeyka <vorobeyka3222@gmail.com>
2020-10-13 16:30:18 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+export PKG_CONFIG_PATH="-L/.brew/Cellar/qt/5.15.1/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="~/.brew/Cellar/qt/5.15.1//lib/cmake:$CMAKE_PREFIX_PATH"
+
 cmake . -Bbuild -Wdev -Werror=dev && cmake --build ./build
 
 cp ./build/utext .
